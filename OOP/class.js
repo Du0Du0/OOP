62742f79b1567c966a525d07926dc1bd01c95375
do-hyun,kim <dohyun911@hanmail.net>
2023-09-21 11:33:23 +0900

    using static with oop

--- OOP/class.js
+++ OOP/class.js
@@ -1,24 +1,24 @@
 {
-    var CoffeeMaker = /** @class */ (function () {
+    var CoffeeMaker_1 = /** @class */ (function () {
         function CoffeeMaker(coffeeBeans) {
-            this.BEANS_GRAMN_PER_SHOT = 7;
-            this.coffeeBeans = 0;
+            this.coffeeBeans = 0; // instance (object) level
             this.coffeeBeans = coffeeBeans;
         }
         CoffeeMaker.prototype.makeCoffee = function (shots) {
-            if (this.coffeeBeans < shots * this.BEANS_GRAMN_PER_SHOT) {
+            if (this.coffeeBeans < shots * CoffeeMaker.BEANS_GRAMN_PER_SHOT) {
                 throw new Error('Not enough coffee beans!');
             }
-            this.coffeeBeans -= shots * this.BEANS_GRAMN_PER_SHOT;
+            this.coffeeBeans -= shots * CoffeeMaker.BEANS_GRAMN_PER_SHOT;
             return {
                 shots: shots,
                 hasMilk: false,
             };
         };
+        CoffeeMaker.BEANS_GRAMN_PER_SHOT = 7; //static을 붙이면 class level
         return CoffeeMaker;
     }());
-    var maker = new CoffeeMaker(32);
+    var maker = new CoffeeMaker_1(32);
     console.log(maker);
-    var maker2 = new CoffeeMaker(14);
+    var maker2 = new CoffeeMaker_1(14);
     console.log(maker2);
 }
--- OOP/class.ts
+++ OOP/class.ts
@@ -5,18 +5,22 @@
 	};
 
 	class CoffeeMaker {
-		BEANS_GRAMN_PER_SHOT: number = 7;
-		coffeeBeans: number = 0;
+		static BEANS_GRAMN_PER_SHOT: number = 7; //static을 붙이면 class level
+		coffeeBeans: number = 0; // instance (object) level
 
 		constructor(coffeeBeans: number) {
 			this.coffeeBeans = coffeeBeans;
 		}
 
+		static makeMachine(coffeeBeans: number): CoffeeMaker {
+			return new CoffeeMaker(coffeeBeans);
+		}
+
 		makeCoffee(shots: number): CoffeeCup {
-			if (this.coffeeBeans < shots * this.BEANS_GRAMN_PER_SHOT) {
+			if (this.coffeeBeans < shots * CoffeeMaker.BEANS_GRAMN_PER_SHOT) {
 				throw new Error('Not enough coffee beans!');
 			}
-			this.coffeeBeans -= shots * this.BEANS_GRAMN_PER_SHOT;
+			this.coffeeBeans -= shots * CoffeeMaker.BEANS_GRAMN_PER_SHOT;
 			return {
 				shots,
 				hasMilk: false,
@@ -27,4 +31,6 @@
 	console.log(maker);
 	const maker2 = new CoffeeMaker(14);
 	console.log(maker2);
+
+	CoffeeMaker.makeMachine(3);
 }
